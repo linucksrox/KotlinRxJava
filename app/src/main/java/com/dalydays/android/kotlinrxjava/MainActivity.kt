b4ab8345a0fe0d345a17a8a435eac2c467f5e0d9
linucksrox <ericdalyblog@dalydays.com>
2018-03-16 06:58:44 -0400

    Initial commit - with some TODOs

---
+++ app/src/main/java/com/dalydays/android/kotlinrxjava/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.dalydays.android.kotlinrxjava
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        TODO("Create a new function which creates an observable")
+        TODO("Subscribe to the observable")
+        TODO("Run the observable code on a separate thread")
+        TODO("Unsubscribe in onDestroy()")
+    }
+}
